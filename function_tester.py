51251e79e4467cadceaee094a39349485f45da49
ChrisHSMol <61701870+ChrisHSMol@users.noreply.github.com>
2023-03-11 23:03:13 +0100

    Tælletræ oprettet

--- function_tester.py
+++ function_tester.py
@@ -1,5 +1,6 @@
 from manim import *
 from helpers import *
+import numpy as np
 
 slides = False
 if slides:
@@ -11,3 +12,23 @@ class Keyword(Slide if slides else Scene):
         self.add(NumberPlane())
         keyword_overlay(self)
         slides_pause(self, 5)
+
+
+class BezierSplineExample(Scene):
+    def construct(self):
+        p1 = np.array([0, 1, 0])
+        p1b = p1 + [-0.5, 0, 0]
+        d1 = Dot(point=p1).set_color(BLUE)
+        l1 = Line(p1, p1b)
+        p2 = np.array([-2, -1, 0])
+        p2b = p2 + [0, 0.5, 0]
+        d2 = Dot(point=p2).set_color(RED)
+        l2 = Line(p2, p2b)
+        bezier = CubicBezier(p1b, p1b + LEFT, p2b + UP, p2b)
+        self.add(
+            l1,
+            d1,
+            l2,
+            d2,
+            bezier
+        )
