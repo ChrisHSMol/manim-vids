b7f644cc9f2cf6549d4ec86eba81c87f26f888a6
ChrisHSMol <61701870+ChrisHSMol@users.noreply.github.com>
2023-04-04 19:39:13 +0200

    lk

--- function_tester.py
+++ function_tester.py
@@ -40,16 +40,49 @@ class BezierSplineExample(Scene):
         xl_pause(self)
 """
 
-cec = []
-chr = []
-with open("data.csv", "r") as inFile:
-    for line in inFile:
-        line = line.split()
-        if line[0] == "Cecilie":
-            cec.append(int(line[1]))
-        elif line[0] == "Christoffer":
-            chr.append(int(line[1]))
+# cec = []
+# chr = []
+# with open("data.csv", "r") as inFile:
+#     for line in inFile:
+#         line = line.split()
+#         if line[0] == "Cecilie":
+#             cec.append(int(line[1]))
+#         elif line[0] == "Christoffer":
+#             chr.append(int(line[1]))
+#
+# print(len(cec), len(chr))
+# for i in sorted(chr):
+#     print(int(i))
 
-print(len(cec), len(chr))
-for i in sorted(chr):
-    print(int(i))
+from manim import *
+# from .manim_physics.src.manim_physics import *
+from manim_physics import *
+
+
+# use a SpaceScene to utilize all specific rigid-mechanics methods
+class TwoObjectsFalling(SpaceScene):
+    def construct(self):
+        circle = Circle().shift(UP)
+        circle.set_fill(RED, 1)
+        circle.shift(DOWN + RIGHT)
+
+        rect = Square().shift(UP)
+        rect.rotate(PI / 4)
+        rect.set_fill(YELLOW_A, 1)
+        rect.shift(UP * 2)
+        rect.scale(0.5)
+
+        ground = Line([-4, -3.5, 0], [4, -3.5, 0])
+        wall1 = Line([-4, -3.5, 0], [-4, 3.5, 0])
+        wall2 = Line([4, -3.5, 0], [4, 3.5, 0])
+        walls = VGroup(ground, wall1, wall2)
+        self.add(walls)
+
+        self.play(
+            DrawBorderThenFill(circle),
+            DrawBorderThenFill(rect),
+        )
+        self.make_rigid_body(rect, circle)  # Mobjects will move with gravity
+        self.make_static_body(walls)  # Mobjects will stay in place
+        self.wait(5)
+        # during wait time, the circle and rect would move according to the simulate updater
